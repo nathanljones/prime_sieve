1e389f7ff39aef9d5f11c4734911eea1b2813aae
nathanljones <nathan.l.jones+github@protonmail.com>
2024-08-10 12:49:31 +0100

    Initial Check In

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Initial Implementation of the Sieve of Eratosthenes.
+
+Note - this is a first pass and will require optimisation and making more rust like
---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use std::{cmp::max, f32::consts::SQRT_2};
+
+fn main() {
+    println!("Hello, world!");
+    prime_sieve(200);
+}
+
+fn prime_sieve(max_no: u32) {
+    let mut primes = vec![true; max_no.try_into().unwrap()];
+    let limit = max_no;
+    /*let limit = max_no as f32;
+    let limit = limit.sqrt();
+    let limit = limit as u32;*/
+
+    for n in 1..=primes.len() {
+        if n % 2 == 0 {
+            primes[(n - 1) as usize] = false;
+        }
+    }
+
+    for n in (3..=limit).step_by(2) {
+        for j in (n * n..=max_no) {
+            if j % n == 0 {
+                primes[(j - 1) as usize] = false;
+            }
+        }
+    }
+    for n in 1..=primes.len() {
+        if primes[n - 1 as usize] == true {
+            println!("{}", n);
+        }
+    }
+}
