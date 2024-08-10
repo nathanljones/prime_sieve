a69a3d8035d76626e733a7c3f481f8d53a2db096
nathanljones <nathan.l.jones+github@protonmail.com>
2024-08-10 13:47:55 +0100

    further optimisations and clippy tweaks

--- src/main.rs
+++ src/main.rs
@@ -6,23 +6,28 @@ fn main() {
 fn prime_sieve(max_no: u32) {
     let mut primes = vec![true; max_no.try_into().unwrap()];
     let limit = max_no;
-    /*let limit = max_no as f32;
-    let limit = limit.sqrt();
-    let limit = limit as u32;*/
 
-    for n in 1..=primes.len() {
+    // remove all the 2 ones first - treat this as a special case
+    for n in (4..=primes.len()).step_by(2) {
         if n % 2 == 0 {
             primes[n - 1] = false;
         }
     }
 
     for n in (3..=limit).step_by(2) {
+        // if we encountered a false value then we can skip as this is
+        // just a multiple of one which has been found
+        if !primes[(n - 1) as usize] {
+            break;
+        }
+        // main sieve check
         for j in n * n..=max_no {
             if j % n == 0 {
                 primes[(j - 1) as usize] = false;
             }
         }
     }
+    // now write out all our prime numbers
     for n in 1..=primes.len() {
         if primes[n - 1] {
             println!("{}", n);
